da3a4455fe528d40a9dd07dff72d8bf25d8f3ccb
braile <andre.prze.filho@gmail.com>
2022-03-24 19:08:22 -0300

    included dependencies in setup

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     description="Mapless Monte-Carlo Localization implementation approaches",
     package_dir={"" : "src"},
     packages=["mapless_mcl", "mapless_demos"],
-    install_requires = ["numpy>=1.18.5", "shapely", "pyproj", "geopandas", "utm", "scipy", "matplotlib", "python-dateutil>=2.8.1"]
+    install_requires = ["numpy>=1.18.5", "shapely", "pyproj", "rtree", "geopandas", "utm", "scipy", "matplotlib", "python-dateutil>=2.8.1"]
 )
